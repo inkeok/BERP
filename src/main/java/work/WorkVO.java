612a559941aeb4e465423e708538dd44ecad9198
imSeungJin23 <109931390+imSeungJin23@users.noreply.github.com>
2022-11-11 16:28:03 +0900

    11.11 16:28

---
+++ src/main/java/work/WorkVO.java
@@ -0,0 +1,13 @@
+package work;
+
+import java.util.Date;
+
+public class WorkVO {
+
+	private String company_cd, excess_date, work_code; 
+	private int employee_id, department_id;
+	private Date  start_work, end_work, work_date; 
+	 
+	
+	
+}
