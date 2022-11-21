85fbd2f3a248fd6cf2ac22da7544b25c85fa0e7f
yoonyounggun <yyg6197@naver.com>
2022-11-21 17:19:42 +0900

    2022.11.21.

--- src/main/java/com/hanul/berp/MyPageController.java
+++ src/main/java/com/hanul/berp/MyPageController.java
@@ -6,7 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.google.gson.Gson;
+
+import member.AndLoginMemberVO;
 import mypage.EmpMemberVO;
 import mypage.MemberVO;
 import mypage.MyPageDAO;
@@ -51,4 +55,36 @@ public class MyPageController {
 		return "side/myPage/info";
 	}
 	
+	
+///////////////////////////////////안드로이드/////////////////////////////////////////////////////////
+	
+/*	
+@ResponseBody @RequestMapping(value="/andModify.mypage", produces="text/html; charset=utf-8")
+public String AndLoginCheck(String id, String pw) {
+Boolean info = false;
+List<AndLoginMemberVO> memList = dao.andCheckLogin();
+System.out.println(memList.get(0).getEmployee_id());
+for (int i = 0; i < memList.size(); i++) {
+if(memList.get(i).getEmployee_id() == Integer.parseInt(id) && memList.get(i).getPw().equals(pw)) {
+info = true;
+break;
+}else {
+continue;
+}
+}
+
+if(!info) {
+return "zzz";
+}else{
+List<AndLoginMemberVO> loginList = dao.andLogin(id);
+
+return new Gson().toJson(loginList);
+}
+
+
+
+}
+	*/ 
+
+//dto 패스워드 추가 확인 
 }
