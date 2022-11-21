1eef0679a1ab4e25de36a173d75541607a9ad983
ooisv@naver.com <ooisv@naver.com>
2022-11-21 15:13:43 +0900

    11-21 14:38 lsb
    .

--- src/main/java/com/hanul/berp/CommonController.java
+++ src/main/java/com/hanul/berp/CommonController.java
@@ -2,8 +2,6 @@ package com.hanul.berp;
 
 import java.util.List;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,22 +16,67 @@ public class CommonController {
 	@Autowired CommonDAO dao;
 	
 	@RequestMapping("/common.cd")
-	public String list(Model model, HttpSession session
-			,@RequestParam(defaultValue = "all") String code_title) {
+	public String list(Model model
+			,@RequestParam(defaultValue = "") String code, CommonVO vo
+//			,@RequestParam(defaultValue = "all") String code_title
+//			,@RequestParam(defaultValue = "all") String code_value
+//			,@RequestParam(defaultValue = "all") String code_used
+//			,@RequestParam(defaultValue = "all") String code_name
+			) {
 		
+		//code_title : 인사코드, 	code_value : 문서코드,	code_used : 고용형태코드,	code_name : 근무코드
+//		String code_title = vo.getCode_title();
+//		String code_value = vo.getCode_value();
+//		String code_used = vo.getCode_used();
+//		String code_name = vo.getCode_name();
+//		vo.setCode_title("all");
+//		vo.setCode_value("all");
+//		vo.setCode_used("all");
+//		vo.setCode_name("all");
+		if( code.equals("code_title") ) {
+			model.addAttribute("code_title", vo.getCode_title());  //선택한 코드
+		}else if( code.equals("code_value") ) {
+			model.addAttribute("code_value", vo.getCode_value());  //선택한 코드
+		}else if( code.equals("code_used") ) {
+			model.addAttribute("code_used", vo.getCode_used());  //선택한 코드
+		}else if( code.equals("code_name") ) {
+			model.addAttribute("code_name", vo.getCode_name());  //선택한 코드
+		}
 		
-		List<CommonVO> commonlist = dao.Common_list();
+		List<CommonVO> code_titles = dao.personal_code(); //인사코드 목록(드랍다운목록)
+		model.addAttribute("code_titles",code_titles);
+		List<CommonVO> code_values = dao.document_code(); //문서코드 목록(드랍다운목록)
+		model.addAttribute("code_values",code_values);
+		List<CommonVO> code_used = dao.employee_code(); //문서코드 목록(드랍다운목록)
+		model.addAttribute("code_used",code_used);
+		List<CommonVO> code_name = dao.work_code(); //문서코드 목록(드랍다운목록)
+		model.addAttribute("code_name",code_name);
 		
-		List<CommonVO> documentlist = dao.document_list(code_title);
+		List<CommonVO> commonlist = dao.common_list();
+
+//		if(code_title.equalsIgnoreCase("all")) {
+//			list = dao.document_list(code_title);
+//		}else {
+//			System.out.println("안됨");
+//		}
 		
-		if(code_title.equalsIgnoreCase("all")) {
-			commonlist = dao.Common_list();
+//		List<CommonVO> documentlist = dao.document_list();
+		
+//		if(code_title.equalsIgnoreCase("all")) {
+//			commonlist = dao.Common_list();
+		List<CommonVO> list = null;
+		if( code.isEmpty() ) {
+			list = dao.common_list(); //코드 전체 목록
 		}else {
-			documentlist = dao.document_list(code_title);
+//			documentlist = dao.document_list();
+			list = dao.common_list(vo, code); //선택한 코드 전체 목록
 		}
 		
 		model.addAttribute("list", commonlist);
-		model.addAttribute("code_title", documentlist);
+//		model.addAttribute("code_title", documentlist);
+			
+		model.addAttribute("list", list);
+
 		
 		return "side/common/common";
 	}
