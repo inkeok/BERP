bb2b0ff4c9e9af59cf5745dc6e51f59094c692c8
yoonyounggun <yyg6197@naver.com>
2022-11-22 17:33:13 +0900

    dd
    dd

--- src/main/webapp/resources/js/apply.js
+++ src/main/webapp/resources/js/apply.js
@@ -82,7 +82,7 @@
 		,lack: {code:'invalid', desc:'영문 대/소문자, 숫자 모두 포함해'}
 		,valid: {code: 'valid', desc:'사용가능한 비밀번호'}
 		,equal: {code: 'valid', desc:'비번 일치'}
-		,notEqual: {code: 'invalid', desc:'비번 노일치'}
+		,notEqual: {code: 'invalid', desc:'비번가 일치하지 않습니다'}
 	},
 	
 	
