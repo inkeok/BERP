804be174b2bad8eb4070f485c01f1eb1e21754e1
inkeok <109931447+inkeok@users.noreply.github.com>
2022-11-24 15:15:32 +0900

    22.11.24

--- src/main/java/salary/SalaryDAO.java
+++ src/main/java/salary/SalaryDAO.java
@@ -8,15 +8,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
 
-import emp.EmpVO;
 
 @Repository
 public class SalaryDAO {
 	@Autowired @Qualifier("bteam") private SqlSession sql;
 	
-	
-	public List<EmpVO> employee_list(){
+	public List<SalaryVO> salary_List() {
+		return sql.selectList("salary.saList");
+	}
+	public List<SalEmpVO> employee_list(){
 		return sql.selectList("salary.empList");
+	}
+	//부서목록조회
+	public List<SalEmpVO> departments() {
+		return sql.selectList("salary.departments");
+	}
+	public List<SalEmpVO> employee_list_d(int department_id){
+		return sql.selectList("salary.empList_d",department_id);
+	}
+	public SalEmpVO mod_info(int employee_id) {
+		return sql.selectOne("salary.modInfo", employee_id);
 	}
 	
 	
